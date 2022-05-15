0e53db8738da33a7d500bc934dc49bbce734e8e0
rangaraju29139 <rangaraju29139@gmail.com>
2022-05-15 10:31:50 +0530

    conditional components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,34 @@
 
 import './App.css';
-import galaxy from './galaxy.jpg';
 
 
-const dishes = [
-  "biryani",
-  "noodles",
-  "manchuria",
-  "fried rice"
-];
-
-const dishesObjects=dishes.map((dish,i) => ({id: i,title: dish}));
-console.log(dishesObjects);
-//dishes.map((dish) => console.log(dish));
 
 
-function Header(props){
-  console.log(props);
+
+
+function SecretContent(){
   return (
-    <header>
-      <h1>{props.name}'s Restaurant</h1>
-    </header>
+    <h1>This is the secret content....</h1>
   );
 }
 
-function Main(props){
-  console.log(props);
+
+function NormalContent(){
   return (
-    <section>
-      <h2> where {props.adjective} food is being served. </h2>
-      <img src={galaxy} height={200} alt=""></img>
-      <ul style={{textAlign: "left"}}>
-        {props.dishes.map((dish,i) => <li key={i}>{dish}</li>)}
-      </ul>
-    </section>
-  );
-}
-function Footer(props){
-  console.log(props);
-  return (
-    <footer>
-    <p>{props.year}  thank you visit again....</p> 
-    </footer>
+    <h1>This is the normal content available to everyone....</h1>
   );
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Header name="rangaraju"/>
-      <Main adjective="amazing" dishes={dishes}/>
-      <Footer year={new Date().getFullYear()}/>
-    </div>
-  );
+
+function App(props) {
+  
+    if(props.authorised){
+     return  <SecretContent />;
+    }else{
+     return  <NormalContent />
+    }
+    
+
 }
 
 export default App;
